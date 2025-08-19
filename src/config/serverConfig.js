28ea698b93ec6fc0b7f6d4eec31b3d0fc8a09edf
AKDev32 <amankumar005562@gmail.com>
2025-08-20 00:56:35 +0530

    Added env

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -3,3 +3,5 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env file
 
 export const PORT = process.env.PORT || 3000; // Define a PORT variable
+
+export const MONGO_URL = process.env.MONGO_URL;  // define a mongo_url variable
