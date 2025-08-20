3809f49d951f0d74359c3ff11d5e53d192f9d2c1
AKDev32 <amankumar005562@gmail.com>
2025-08-21 00:50:34 +0530

    Added service and repo layer

--- src/services/tweetService.js
+++ src/services/tweetService.js
@@ -9,7 +9,10 @@ export const createTweet = async ({ body }) => {
     if (filter.isProfane(body)) {
         console.log(body);
         console.log(filter.clean(body));
-        throw new Error("Tweet contains blocked words");
+        throw {
+            message: 'Tweet contains blocked words',
+            status: 400
+        }
     }
 
 
